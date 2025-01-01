fb4d33b7f35866c33c8909993d9e1d468922d7e7
Taha Shahid <tahashahid203@gmail.com>
2025-01-01 18:19:31 +0400

    implement a form to update the title of the document

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,19 +1,57 @@
 "use client";
 
-const Document = ({id}: {id: string}) => {
+import { useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+const Document = ({ id }: { id: string }) => {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [updating, startTransition] = useTransition();
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data]);
+
+  const handleUpdate = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input.trim()) {
+      startTransition(async () => {
+        // update the document
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        });
+      });
+    }
+  };
   return (
     <div>
-        <div>
-            {/* update the title */}
+      <div className="flex max-w-6xl mx-auto justify-between pb-5">
+        {/* update the title */}
+        <form onSubmit={handleUpdate} className="flex flex-1 space-x-2">
+          <Input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="bg-white"
+          />
 
-            {/* if isOwner Then Invite User, Delete Document */}
-        </div>
-        <div>
-            {/* manage users */}
-            {/* avatars */}
-        </div>
-        {/* collaborative editor */}
+          <Button type="submit" disabled={updating}>
+            {updating ? "Updating..." : "Update"}
+          </Button>
+        </form>
+
+        {/* if isOwner Then Invite User, Delete Document */}
+      </div>
+      <div>
+        {/* manage users */}
+        {/* avatars */}
+      </div>
+      {/* collaborative editor */}
     </div>
-  )
-}
-export default Document
+  );
+};
+export default Document;
